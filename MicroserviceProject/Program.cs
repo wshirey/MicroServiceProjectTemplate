51f4a191cd0a24ff16b2fe962034dc132d311845
Wally Shirey <wally.shirey@adtrav.com>
2015-11-12 11:22:54 -0600

    initial commit

---
+++ MicroserviceProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace MicroserviceProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureSerilog();
+            HostFactory.Run(x => 
+            {
+                x.Service<Service>();
+                x.UseSerilog();
+            });
+        }
+
+        private static void ConfigureSerilog()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile(@"logs\$safeprojectname$-{Date}.txt")
+                .CreateLogger()
+                .ForContext("ApplicationName", "");
+        }
+    }
+}
---
+++ MicroserviceProject/Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topshelf;
+
+namespace MicroserviceProject
+{
+    public class Service : ServiceControl
+    {
+        public Service()
+        {
+
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
